c518b4f1de41c5111c71f3445897c2fbb99fbc71
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-02-06 01:33:57 +0300

    add the logic for simple calculations

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let number;
+let numberInMemory; 
+let operator;
+
+let display = document.querySelector(".display");
+display.textContent = "";
+
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let clearButton = document.querySelector(".C");
+
 function add(a, b) {
     return a + b;
 }
@@ -36,7 +47,91 @@ function operate(first_num, second_num, operator) {
     }
 }
 
-console.log(operate(15, 5, "+"));
-console.log(operate(15, 5, "-"));
-console.log(operate(15, 5, "*"));
-console.log(operate(15, 5, "/"));
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals).toFixed(decimals);
+  }
+
+function isInt(n) {
+    return n % 1 === 0;
+ }
+ 
+  
+function getNumber(event) {
+    let value = new Number(event.target.textContent);
+    if (number == undefined) {
+        number = value;
+    }
+    else {
+        number = new Number(number.toString() + value.toString());
+    }
+    displayNumber(number);
+}
+
+function getOperator(event) {
+    let value = event.target.textContent;
+    
+    //case for user pressing on = after inputting 2 numbers and an operator
+    if (value == "=" && number != undefined && numberInMemory != undefined && operator != undefined) {
+        numberInMemory = operate(numberInMemory, number, operator);
+        number = undefined;
+        operator = undefined;
+        displayNumber(numberInMemory);
+        console.log(numberInMemory, number, operator);
+    }
+
+    //case for user pressing on an operator after inputting the first number
+    else if (numberInMemory == undefined && number != undefined) {
+        operator = value;
+        numberInMemory = number;
+        number = undefined;
+    }
+
+    //case for user pressing on an operator after inputting 2 numbers and an operator (calculates the previous operation and uses it as the first number)
+    else if (number != undefined && numberInMemory != undefined && operator != undefined) {
+        numberInMemory = operate(numberInMemory, number, operator);
+        operator = value;
+        number = undefined;
+        displayNumber(numberInMemory);
+    }
+
+    //case for user pressing on an operator after using the = button
+    else if (numberInMemory != undefined && number == undefined) {
+        operator = value;
+    }
+    
+    //case for user inputting new number after getting a result (fresh start lol)
+    else if (numberInMemory != undefined && number != undefined && operator == undefined) {
+        numberInMemory = number;
+        number = undefined;
+        operator = value;
+        displayNumber(numberInMemory);    
+    }
+
+}
+
+function clearDisplay() {
+    number = undefined;
+    operator = undefined;
+    numberInMemory = undefined;
+    display.textContent = "";
+
+}
+
+function displayNumber(numberToDisplay) {
+    if (isInt(numberToDisplay) == false) {
+        numberToDisplay = round(numberToDisplay, 2);
+    }
+    display.textContent = numberToDisplay.toString();
+}
+
+numbers = document.querySelectorAll(".number");
+numbers.forEach((element) => {
+    element.addEventListener("click", getNumber);
+});
+
+operators = document.querySelectorAll(".operator");
+operators.forEach((element) => {
+    element.addEventListener("click", getOperator);
+})
+
+clearButton.addEventListener("click", clearDisplay);
